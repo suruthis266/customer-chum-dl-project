1e62fd23a1f7f00e6720baa0570c48bf015f592d
suruthis266 <suruthis266@gmail.com>
2025-10-27 02:26:53 +0000

    import of python packages

---
+++ models/train_ann.py
@@ -0,0 +1,9 @@
+import pandas as pandas
+import numpy as numpy
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
+
